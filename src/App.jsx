929b038acace78314f314f7a843fbe42cce229e7
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-05-08 18:18:59 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <header></header>
-    </>
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://site--deliveroo-backend--btw6jz59pgz8.code.run/"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data);
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div>
+      <span>{}</span>
+      <br />
+      <ul>{}</ul>
+    </div>
   );
 }
 
