a4fe27b3a4ed54e380475b0baed240a87531782c
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-05-12 11:17:44 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,52 @@ import logo from "./assets/img/deliveroo-logo.png";
 function App() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (meal) => {
+    const newCart = [...cart];
+    let exists = false;
+    //Vérifier si le plat est déjà dans le panier
+    for (let i = 0; i < newCart.length; i++) {
+      if (newCart[i].title === meal.title) {
+        newCart[i].quantity += 1; //Si oui augmenter la quantité
+        exists = true;
+        break;
+      }
+    }
+
+    // Si le plat n'est pas déjà dans le panier, l'y ajouter avec une quantité de 1
+    if (!exists) {
+      newCart.push({ ...meal, quantity: 1 });
+    }
+    setCart(newCart);
+  };
+
+  const incrementMeal = (meal) => {
+    const newCart = [...cart];
+    for (let i = 0; i < newCart.length; i++) {
+      if (newCart[i].title === meal.title) {
+        newCart[i].quantity += 1;
+        break;
+      }
+    }
+    setCart(newCart);
+  };
+
+  const decrementMeal = (meal) => {
+    const newCart = [...cart];
+    for (let i = 0; i < newCart.length; i++) {
+      if (newCart[i].title === meal.title) {
+        if (newCart[i].quantity > 1) {
+          newCart[i].quantity -= 1;
+        } else {
+          newCart.splice(i, 1);
+        }
+        break;
+      }
+    }
+    setCart(newCart);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,6 +73,30 @@ function App() {
     <span>En cours de chargement...</span>
   ) : (
     <div>
+      <div className="cart">
+        {cart.map((item, index) => (
+          <div key={index}>
+            <p>{item.title}</p>
+            <div className="quantity-control">
+              <button onClick={() => decrementMeal(item)}>-</button>
+              <p>{item.quantity}</p>
+              <button onClick={() => incrementMeal(item)}>+</button>
+            </div>
+            <p>{item.price * item.quantity} €</p>
+          </div>
+        ))}
+        {cart.reduce((acc, item) => acc + item.price * item.quantity, 0) >
+          0 && (
+          <p>
+            Total :{" "}
+            {cart
+              .reduce((acc, item) => acc + item.price * item.quantity, 0)
+              .toFixed(2)}{" "}
+            €
+          </p>
+        )}
+      </div>
+
       <div className="header">
         <img src={logo} alt="Header" />
       </div>
@@ -37,13 +107,11 @@ function App() {
             {data.restaurant.description}
           </p>
         </div>
-        {
-          <img
-            src={data.restaurant.picture}
-            alt={data.restaurant.name}
-            className="restaurant-image"
-          />
-        }
+        <img
+          src={data.restaurant.picture}
+          alt={data.restaurant.name}
+          className="restaurant-image"
+        />
       </div>
       {/* Affichage des catégories et des plats */}
       {data.categories.map((category) => (
@@ -51,12 +119,15 @@ function App() {
           <h2>{category.name}</h2>
           <div className="meals">
             {category.meals.map((meal) => (
-              <div className="meal" key={meal.id}>
+              <div
+                className="meal"
+                key={meal.id}
+                onClick={() => addToCart(meal)}
+              >
                 <div className="meal-text">
                   <h3>{meal.title}</h3>
                   <p className="meal-price">{meal.price} €</p>
-
-                  {/* <p className="meal-description">{meal.description}</p> */}
+                  <p className="meal-description">{meal.description}</p>
                 </div>
                 <div className="meal-img">
                   <img
